d042ce3a5bdb7f1811d2abb8b8e59ae8decf2ea8
Seu Nome <139656791+PedroAlvesDev@users.noreply.github.com>
2025-01-09 23:00:18 -0300

    todo-app

--- script.js
+++ script.js
@@ -1,25 +1,49 @@
 let newTask = document.querySelector('input#task');
 let list = document.querySelector('.task-box');
 
-function AddTask() {
-    if(newTask.value.length == 0) {
+function addTask() {
+    let taskValue = newTask.value.trim();
+
+    if(taskValue.length == 0) {
         alert(`Digite uma tarefa válida!`);
     } else {
-        let ulTask = document.createElement('ul');
-        let liTask = document.createElement('li');
-        let inputCheckboxTask = document.createElement('input');
-        inputCheckboxTask.type = 'checkbox';
-        let spanTask = document.createElement('span');
-        spanTask.innerText = newTask.value;
+        let labelTask = document.createElement('label');
+        labelTask.classList.add('task-item');
 
-        list.appendChild(ulTask);;
-        ulTask.appendChild(liTask);
-        liTask.appendChild(inputCheckboxTask);
-        liTask.appendChild(spanTask);
-  
+        let inputTask = document.createElement('input');
+        inputTask.type = 'checkbox';
+
+        let parTask = document.createElement('p');
+        parTask.innerText = taskValue;
+
+        let buttonTrash = document.createElement('button');
+        buttonTrash.classList.add('delete-task');
+
+        let trashItem = document.createElement('i');
+        trashItem.classList.add('fa-solid');
+        trashItem.classList.add('fa-trash-can');
+
+        labelTask.appendChild(inputTask);
+        labelTask.appendChild(parTask);
+        buttonTrash.appendChild(trashItem);
+        labelTask.appendChild(buttonTrash);
+
+        list.appendChild(labelTask);
+
+        trashItem.addEventListener('click', deleteTask);
+        list.addEventListener('click', completeTask);
     };
     
     newTask.value = '';
     newTask.focus();
+};
+function deleteTask(e) {
+    let addTask = e.target.closest('.task-item');
+    addTask.remove();
+};
 
+function completeTask(e) {
+    if(e.target.type === 'checkbox') {
+        e.target.closest('.task-item').classList.toggle('complete');
+    };
 };
